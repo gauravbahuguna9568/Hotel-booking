f43dc32a933e7b2ae42ce2798348f5c33a5ef950
Gaurav Bahuguna <gauravbahuguna9568@gmail.com>
2023-07-27 22:38:52 +0530

    booking part 40% done

--- src/app/bookings/bookings.component.ts
+++ src/app/bookings/bookings.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Bookings } from '../booking.model';
+import { HotelsService } from '../services/hotels.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bookings',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./bookings.component.css']
 })
 export class BookingsComponent {
+
+  bookings:Bookings[]=[];
+
+  constructor(private service:HotelsService,private router:Router){}
+
+  ngOnInit():void{
+    this.service.list().subscribe(data=>this.bookings);
+  }
+  
 
 }
--- src/app/services/hotels.service.ts
+++ src/app/services/hotels.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Customer } from '../customer.model';
 import { Hotels } from '../hotels.model';
+import { Bookings } from '../booking.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
   private static baseUrl="http://localhost:3000";
-  hotelBooking : Customer[]=[];
+  hotelBooking : Customer = new Customer();
   hotel: Hotels[]=[];
+  book: Bookings =new Bookings();
 
   constructor(private http: HttpClient ) { }
   //This is for registering the user 
@@ -30,4 +32,9 @@ searchByCity(city:string){
   return this.http.get<Hotels[]>(HotelsService.baseUrl+"/hotel?city="+city);
 
 }
+// listBooking(bookingId: number):Customer[]{
+//    let bookings :Bookings= new Bookings();
+//    this.http.get<Bookings>(HotelsService.baseUrl+"/hotels/"+bookingId).subscribe(data=>bookings=data);
+//    return this.hotelBooking.bookings
+// }
 }
